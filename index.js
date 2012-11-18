57bf9460472fc34964f566ea7e9206ba8b859019
Mirco <mirco.zeiss@gmail.com>
2012-11-18 14:47:26 +0100

    return early and better errors

--- index.js
+++ index.js
@@ -11,16 +11,27 @@ var options = {
 };
 
 module.exports = function(privateKey, remoteIP, challenge, response, cb) {
+  var error = null;
   
-  if (response === '') {
-    cb(new Error('response must not be empty'));
+  if (!privateKey) {
+    error = 'Private key is required';
+  } else if (!remoteIP) {
+    error = 'Remote IP is required';
+  } else if (!challenge) {
+    error = 'Challenge is required';
+  } else if (!response) {
+    error = 'Response is required';
+  }
+  
+  if (error) {
+    return cb(new Error(error));
   }
   
   var request = http.request(options, function(response) {
     var body = '';
     
     response.on('error', function(err) {
-      cb(new Error(err));
+      return cb(new Error(err));
     });
     
     response.on('data', function(chunk) {
@@ -30,17 +41,14 @@ module.exports = function(privateKey, remoteIP, challenge, response, cb) {
     response.on('end', function() {
       var success = body.split('\n')[0] === "true";
       var error = body.split('\n')[1];
-      if (!success) {
-        cb(new Error(error));
-      } else {
-        cb(null);
-      }
+      if (!success) return cb(new Error(error));
+      cb(null);
     });
     
   });
   
   request.on('error', function(err) {
-    cb(new Error(err));
+    return cb(new Error(err));
   });
   
   var query = 'privatekey=' + privateKey + 
